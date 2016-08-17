46b802f2c48daa331f373ffca7727872bedff7cb
Sara Nasr <snasr@gsngames.com>
2016-08-17 09:52:21 -0700

    Added animation, and more to the next section

---
+++ Assets/Scripts/Pipe001Entry.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe001Entry : MonoBehaviour {
+	GameObject PipeEntry;
+	int StoodOn;
+
+	void OnTriggerEnter(Collider col){
+		StoodOn = 1;
+	}
+
+	void OnTriggerExit(Collider col){
+		StoodOn = 0;
+	}
+
+	void Update(){
+		if (Input.GetKey ("GoDown")) {
+			if (StoodOn == 1) {
+				transform.position = Vector3 (0, -1000, 0);
+				WaitingForPipe ();
+			}
+		}
+	}
+
+	void WaitingForPipe(){
+		PipeEntry.GetComponent<Animator>.
+	}
+}
